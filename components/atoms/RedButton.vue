47995cb0f8b93f058aa2c6c2e6adf557d105dceb
maryam kits <maryam.kits@gmail.com>
2020-09-15 23:19:19 +0400

    modify: red button hover

--- components/atoms/RedButton.vue
+++ components/atoms/RedButton.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     href="/"
-    class="[ py-1-0 px-2-0 bg-red text-white rounded-2-0 inline-block md:px-1-5 md:py-0-5 xs:text-1-0 ]"
+    class="[ py-1-0 px-2-0 bg-red text-white rounded-2-0 inline-block border-red md:px-1-5 md:py-0-5 xs:text-1-0 hover:bg-white hover:text-red transition duration-700 ease-in ]"
   >
     {{ title }}
   </a>
